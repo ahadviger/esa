f0d45b160657d3485eca5ba38b3c4b0bd4e413c5
ahadviger <antea.hadviger@gmail.com>
2015-06-06 22:46:50 +0200

    lint

--- childtable.hpp
+++ childtable.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 class ChildTable {
-	public:
-		int up, down, nextlIndex;
-		ChildTable() {}
-		ChildTable(int _up, int _down, int _nextlIndex) {
-			up = _up; down = _down; nextlIndex = _nextlIndex;
-		}
+ public:
+     int up, down, nextlIndex;
+     ChildTable() {}
+     ChildTable(int _up, int _down, int _nextlIndex) {
+         up = _up; down = _down; nextlIndex = _nextlIndex;
+     }
 };
--- lcpinterval.hpp
+++ lcpinterval.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 class LcpInterval {
-	public:
-		int lcp, Lb, Rb;
-		LcpInterval() {}
-		LcpInterval(int _lcp, int _Lb, int _Rb) {
-			lcp = _lcp; Lb = _Lb, Rb = _Rb;
-		}
+ public:
+     int lcp, Lb, Rb;
+     LcpInterval() {}
+     LcpInterval(int _lcp, int _Lb, int _Rb) {
+         lcp = _lcp; Lb = _Lb, Rb = _Rb;
+     }
 };
